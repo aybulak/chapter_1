30a626d5428e2409750475f1d0ed34b6fe61dfbd
Aybulak <aybulakheber216@gmail.com>
2023-04-26 12:27:41 +0200

    CydeoStudnts selfPractice

---
+++ src/day21selfPractice/cyberStudents.java
@@ -0,0 +1,32 @@
+package day21selfPractice;
+
+import java.util.Arrays;
+
+public class cyberStudents {
+
+    public static void main(String[] args) {
+
+
+    String []Batch2Group1={"Ahmet","Batur","Beyza","Ali"};
+    String []Batch2Group2={"Burcu","Simona","Aynur","Hanna","Christina"};
+    String []Batch2Group3={"BO","Abdulla","Salih","Max"};
+    String[]batch24Group={"Alya","Roshan","Kadir","Aysel","Christina","Maria"};
+
+    String[][]Batch2Group ={Batch2Group1,Batch2Group2,Batch2Group3};
+
+    String[][]CyberStudents={ Batch2Group1,Batch2Group2,Batch2Group3,batch24Group};
+        System.out.println(Arrays.deepToString(Batch2Group));
+        System.out.println(Arrays.deepToString(CyberStudents));
+        System.out.println(Arrays.deepToString(batch24Group));
+
+
+        for (String[] eachBatch : CyberStudents) {
+            for (String eachStudent : eachBatch) {
+                System.out.print("CydeoStudents:" + eachStudent);
+                System.out.println();
+            }
+
+        }
+
+}
+}
